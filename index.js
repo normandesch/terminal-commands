5a85bbebdeefbc440050c05963a3cd5248bebcd5
Norman E Desch III <45925700+normandesch@users.noreply.github.com>
2019-05-06 22:55:56 -0400

    corrected
    changed code to have it run properly.

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const directory = './path/';
 module.exports.ls = () => {
   fs.readdir('./', (err, files) => {
     const filesToString = files.reduce((acc, file) => {
-      return `${acc} ${file} `;
-    }, '');
+    return `${acc} ${file} `;
+  }, '');
 
-    console.log(filesToString);
-  });
+  console.log(filesToString);
+ });
 };
 
-module.exports.touch = () => {
-  fs.closeSync(fs.openSync(filename, 'a'));
+module.exports.touch = (name) => {
+  fs.writeFile(name, message, (err) => {
+     if (err)
+	     return console.log(`${name} was created`);
+
+  console.log(file);
 };
 
+module.exports.mkdir = (dir) => {
+  fs.mkdir(dir, (err) => {
+   if (err)
+      return console.error(err);
 
-module.exports.mkdir = () => {
-  if (!fs.existsSync(directory)) {
-      fs.mkdirSync(directory);
-      console.log('Directory created');
-    } else {
-      console.log('Directory already exists');
-    }
-  };
+   console.log(`${dir} was created`)
+};
